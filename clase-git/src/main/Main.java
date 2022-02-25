fae45d7a9579702e1fe80865ea7eeec3dad3a6ac
AndresTGonzalez <atapia7646@uta.edu.ec>
2022-02-24 21:49:15 -0500

    creados metodos de suma y resta

--- clase-git/src/main/Main.java
+++ clase-git/src/main/Main.java
@@ -4,10 +4,57 @@
  */
 package main;
 
+import java.util.Scanner;
+
 /**
  *
  * @author andre
  */
 public class Main {
-    
+
+    public static void main(String[] args) {
+        int op;
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.printf("%s\n%s\n%s\n%s\n%s\n\n",
+                    "1. Sumar",
+                    "2. Restar",
+                    "3. Multiplicar",
+                    "4. Dividir",
+                    "5. Salir");
+            System.out.printf("%s", "Opción: ");
+            op = Integer.valueOf(sc.nextLine());
+            if (op != 5) {
+                int resultado, num1, num2;
+                float respuesta;
+                switch (op) {
+                    case 1:
+                        //Suma
+                        System.out.printf("%s", "Número 1: ");
+                        num1 = Integer.valueOf(sc.nextLine());
+                        System.out.printf("%s", "Número 2: ");
+                        num2 = Integer.valueOf(sc.nextLine());
+                        resultado = num1 + num2;
+                        System.out.println("Respuesta: " + resultado);
+                        break;
+                    case 2:
+                        //Resta
+                        System.out.printf("%s", "Número 1: ");
+                        num1 = Integer.valueOf(sc.nextLine());
+                        System.out.printf("%s", "Número 2: ");
+                        num2 = Integer.valueOf(sc.nextLine());
+                        resultado = num1 - num2;
+                        System.out.println("Respuesta: " + resultado);
+                        break;
+                    case 3:
+                        //Multiplicacion
+                        break;
+                    case 4:
+                        //Division
+                        break;
+                }
+            }
+        } while (op != 5);
+    }
 }
